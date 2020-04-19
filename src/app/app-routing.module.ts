bb4816f1b7306dcc419612659941eb204890273c
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-19 11:57:50 +0200

    Add components students, courses and exams

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { ExamsComponent } from './components/exams/exams.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { StudentsComponent } from './components/students/students.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'students', component: StudentsComponent},
+  {path: 'courses', component: CoursesComponent},
+  {path: 'exams', component: ExamsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './components/students/students.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { ExamsComponent } from './components/exams/exams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    CoursesComponent,
+    ExamsComponent
   ],
   imports: [
     BrowserModule,
