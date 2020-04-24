3f21a7c55d2ff2a1d964885f40e7c265568919df
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-24 13:47:28 +0200

    Add Courses list

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,15 +1,19 @@
+import { CourseService } from './../../services/course.service';
+import { Course } from './../../models/course';
 import { Component, OnInit } from '@angular/core';
+import { CommonListComponent } from '../common-list.component';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent implements OnInit {
+export class CoursesComponent extends CommonListComponent<Course, CourseService> implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(service: CourseService) {
+    super(service);
+    this.title = 'Courses list';
+    this.modelName = Course.name;
   }
 
 }
--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,12 +1,18 @@
+import { BASE_ENDPOINT } from './../config/app';
 import { Course } from './../models/course';
 import { Injectable } from '@angular/core';
 import { CommonService } from './common.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService extends CommonService<Course> {
 
-  protected baseEndpoint = 'http://localhost:8090/api/courses';
+  protected baseEndpoint = BASE_ENDPOINT + '/courses';
+
+  constructor(httpClient: HttpClient) {
+    super(httpClient);
+  }
 
 }
