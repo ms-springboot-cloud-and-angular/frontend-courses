ef345dcde0bee1646b77d63df7d92247440bceca
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-05-01 12:14:35 +0200

    Add Questions in Exam form

--- src/app/components/exams/exam-forms.component.ts
+++ src/app/components/exams/exam-forms.component.ts
@@ -1,9 +1,11 @@
+import Swal from 'sweetalert2'
 import { Subject } from './../../models/subject';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { ExamService } from './../../services/exam.service';
 import { Exam } from 'src/app/models/exam';
 import { CommonFormsComponent } from '../common-forms.component';
+import { Question } from 'src/app/models/question';
 
 @Component({
   selector: 'app-exam-forms',
@@ -15,6 +17,7 @@ export class ExamFormsComponent extends CommonFormsComponent<Exam, ExamService> 
 
   parentSubjects: Subject[] = [];
   childrenSubjects: Subject[] = [];
+  errorQuestions: String;
 
   constructor(service: ExamService,
     protected router: Router,
@@ -43,6 +46,32 @@ export class ExamFormsComponent extends CommonFormsComponent<Exam, ExamService> 
     this.service.findAllSubjects().subscribe(subjects => this.parentSubjects = subjects.filter(s => !s.parent));
   }
 
+  public create(): void {
+    if (this.model.questions.length === 0) {
+      this.errorQuestions = 'The exam must have at least one question';
+      // Swal.fire('Error in questions', 'The exam must have at least one question', 'error');
+      return;
+    }
+    else {
+      this.errorQuestions = undefined;
+    }
+    this.deleteEmptyQuestion();
+    super.create();
+  }
+
+  public edit(): void {
+    if (this.model.questions.length === 0) {
+      //Swal.fire('Error in questions', 'The exam must have at least one question', 'error');
+      this.errorQuestions = 'The exam must have at least one question';
+      return;
+    }
+    else {
+      this.errorQuestions = undefined;
+    }
+    this.deleteEmptyQuestion();
+    super.edit();
+  }
+
   public loadChildrens(): void {
     this.childrenSubjects = this.model.parentSubject ? this.model.parentSubject.children : [];
   }
@@ -54,4 +83,20 @@ export class ExamFormsComponent extends CommonFormsComponent<Exam, ExamService> 
     return (s1 === null || s2 === null || s1 === undefined || s2 === undefined) ? false : (s1.id === s2.id);
   }
 
+  public addQuestion(): void {
+    this.model.questions.push(new Question());
+  }
+
+  public assignText(question: Question, event: any): void {
+    question.text = event.target.value as string;
+    console.log(this.model);
+  }
+
+  public deleteQuestion(question: Question): void {
+    this.model.questions = this.model.questions.filter(p => question.text !== p.text);
+  }
+
+  public deleteEmptyQuestion(): void {
+    this.model.questions = this.model.questions.filter(p => p.text != null && p.text.length > 0);
+  }
 }
