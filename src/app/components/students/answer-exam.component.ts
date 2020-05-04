407731e0f6475a5d55d95b2775ccf1148beaa83d
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-05-04 19:02:16 +0200

    Paginate answer exams and create buttons to respond and view

--- src/app/components/students/answer-exam.component.ts
+++ src/app/components/students/answer-exam.component.ts
@@ -1,10 +1,12 @@
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { CourseService } from './../../services/course.service';
 import { StudentService } from './../../services/student.service';
 import { ActivatedRoute } from '@angular/router';
 import { Exam } from 'src/app/models/exam';
 import { Course } from 'src/app/models/course';
 import { Student } from './../../models/student';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-answer-exam',
@@ -16,7 +18,11 @@ export class AnswerExamComponent implements OnInit {
   student: Student;
   course: Course;
   exams: Exam[] = [];
-  displayedExamsColumns: string[] = ['id', 'name', 'subject', 'questions'];
+  displayedExamsColumns: string[] = ['id', 'name', 'subject', 'questions', 'respond', 'view'];
+
+  dataSource: MatTableDataSource<Exam>;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
 
   constructor(private route: ActivatedRoute,
     private studentService: StudentService,
@@ -30,6 +36,9 @@ export class AnswerExamComponent implements OnInit {
         this.courseService.getCourseByStudentId(this.student).subscribe(course => {
           this.course = course;
           this.exams = (this.course && this.course.exams) ? this.course.exams : [];
+          this.dataSource = new MatTableDataSource<Exam>(this.exams);
+          this.dataSource.paginator = this.paginator;
+          this.paginator._intl.itemsPerPageLabel = 'Rows per page';
         });
       });
     });
